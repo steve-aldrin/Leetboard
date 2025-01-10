309dd22be606ba1f057ef3e51f59007eb934e04a
Muhammad Hisham <mohmadetsh874@gmail.com>
2025-01-10 14:36:43 -0800

    Add-feature: contributing button

--- src/popup/firebase_config.js
+++ src/popup/firebase_config.js
@@ -3,27 +3,18 @@ import { getFirestore } from 'firebase/firestore';
 
 
 // config after registering firebase App 
-const config = {
-    apiKey: "",
-
-    authDomain: "",
-  
-    projectId: "",
-  
-    storageBucket: "",
-  
-    messagingSenderId: "",
-  
-    appId: "",
-  
-    measurementId: ""
-  
-};
-
+const firebaseConfig = {
+    apiKey: "AIzaSyAdAmSLA5-xTnWqJfEPtT5Be4UnKp3XMkw",
+    authDomain: "leetboard-2e3cb.firebaseapp.com",
+    projectId: "leetboard-2e3cb",
+    storageBucket: "leetboard-2e3cb.firebasestorage.app",
+    messagingSenderId: "557091161436",
+    appId: "1:557091161436:web:31681aafd9aed95da86d99"
+  };
 
 // This creates firebaseApp instance
 // version: SDK 9
-const firebaseApp = initializeApp(config)
+const firebaseApp = initializeApp(firebaseConfig)
 const db = getFirestore(firebaseApp);
 
 export{
