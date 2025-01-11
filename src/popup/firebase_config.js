e3ce0b387ec48f556fa510e6f1185ba68ba5755a
Muhammad Hisham <mohmadetsh874@gmail.com>
2025-01-10 20:28:54 -0800

    Fix: bug

--- src/popup/firebase_config.js
+++ src/popup/firebase_config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // config after registering firebase App 
 const firebaseConfig = {
-    apiKey: "AIzaSyAdAmSLA5-xTnWqJfEPtT5Be4UnKp3XMkw",
-    authDomain: "leetboard-2e3cb.firebaseapp.com",
-    projectId: "leetboard-2e3cb",
-    storageBucket: "leetboard-2e3cb.firebasestorage.app",
-    messagingSenderId: "557091161436",
-    appId: "1:557091161436:web:31681aafd9aed95da86d99"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 // This creates firebaseApp instance
